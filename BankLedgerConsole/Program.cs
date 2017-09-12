3797a88e2016fbf2f36eb6720bf71bbc7527bde4
Brad <bradcopenhaver@gmail.com>
2017-09-11 18:34:51 -0700

    Add project files.

---
+++ BankLedgerConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankLedgerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
